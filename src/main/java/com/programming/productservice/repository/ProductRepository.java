f24e8e76bc9cc248f54593b1284c6cfb1f0ce78d
akmal09 <akmalfauzan.af@gmail.com>
2024-08-07 20:36:48 +0700

    git reconfig

--- src/main/java/com/programming/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.programming.productservice.repository;
-
-import com.programming.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
