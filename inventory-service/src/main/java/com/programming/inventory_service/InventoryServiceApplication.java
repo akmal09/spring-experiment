0a12b47e6c918786fea0a3c7ac91a087e86f1b38
akmal09 <akmalfauzan.af@gmail.com>
2024-09-14 17:10:33 +0700

    adjustment

---
+++ inventory-service/src/main/java/com/programming/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,34 @@
+package com.programming.inventory_service;
+
+import com.programming.inventory_service.model.Inventory;
+import com.programming.inventory_service.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13_pro");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
+
+
+}
