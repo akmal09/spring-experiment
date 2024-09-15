de01319789ec5461fbefa345b6c381b2cefb71fa
akmal09 <akmalfauzan.af@gmail.com>
2024-09-15 13:21:35 +0700

    service discovery add

--- inventory-service/src/main/java/com/programming/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programming/inventory_service/InventoryServiceApplication.java
@@ -1,11 +1,9 @@
 package com.programming.inventory_service;
 
-import com.programming.inventory_service.model.Inventory;
-import com.programming.inventory_service.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -14,21 +12,21 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
-		return args -> {
-			Inventory inventory = new Inventory();
-			inventory.setSkuCode("iphone_13");
-			inventory.setQuantity(100);
-
-			Inventory inventory1 = new Inventory();
-			inventory1.setSkuCode("iphone_13_pro");
-			inventory1.setQuantity(0);
-
-			inventoryRepository.save(inventory);
-			inventoryRepository.save(inventory1);
-		};
-	}
+//	@Bean
+//	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+//		return args -> {
+//			Inventory inventory = new Inventory();
+//			inventory.setSkuCode("iphone_13");
+//			inventory.setQuantity(100);
+//
+//			Inventory inventory1 = new Inventory();
+//			inventory1.setSkuCode("iphone_13_pro");
+//			inventory1.setQuantity(0);
+//
+//			inventoryRepository.save(inventory);
+//			inventoryRepository.save(inventory1);
+//		};
+//	}
 
 
 }
